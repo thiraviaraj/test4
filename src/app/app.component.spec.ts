b937d4dabe4e0275008dc8eec61c9c9769b641b0
thiraviaraj <thiraviaraj1990@gmail.com>
2023-04-18 11:14:00 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,7 +26,7 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('test4');
   });
 
-  xit('should render title', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
